c138ec1fa1fc9a34afdbec79326f8c8716026740
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-09-26 16:50:16 -0300

    Initial Commit

---
+++ Spokast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spokast
+//
+//  Created by Rodrigo Cerqueira Reis on 26/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpokastUITests/SpokastUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SpokastUITestsLaunchTests.swift
+//  SpokastUITests
+//
+//  Created by Rodrigo Cerqueira Reis on 26/09/25.
+//
+
+import XCTest
+
+final class SpokastUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
